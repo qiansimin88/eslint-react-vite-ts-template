f2fe23923e1ff2b4adff5e2818c51d1eed584d8b
qsm <348867341@qq.com>
2023-07-18 04:20:37 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+/*
+ * @Author: qsm 348867341@qq.com
+ * @Date: 2023-07-18 03:13:29
+ * @LastEditors: qsm 348867341@qq.com
+ * @LastEditTime: 2023-07-18 04:12:52
+ * @FilePath: /eslint-react-vite-ts-template/.eslintrc.cjs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/* eslint-env node */
+
+module.exports = {
+    root: true,
+    env: { browser: true, es2021: true, node: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:react-hooks/recommended',
+        'plugin:react/jsx-runtime' // jsx运行时 不需要在页面必须引入react
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: true,
+        tsconfigRootDir: __dirname
+    },
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        '@typescript-eslint/no-non-null-assertion': 'off'
+    },
+    settings: {
+        react: {
+            version: 'detect' // 程序自行检测react版本
+        }
+    }
+}
