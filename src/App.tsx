6b64b8020913b71243b92c2fcab131da759567f9
qsm <348867341@qq.com>
2023-07-18 05:01:54 +0800

    init5

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
  * @Author: qsm 348867341@qq.com
  * @Date: 2023-07-18 03:13:29
  * @LastEditors: qsm 348867341@qq.com
- * @LastEditTime: 2023-07-18 04:48:45
+ * @LastEditTime: 2023-07-18 05:01:33
  * @FilePath: /eslint-react-vite-ts-template/src/App.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
