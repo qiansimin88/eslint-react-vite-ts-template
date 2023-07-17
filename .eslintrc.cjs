06825e12603973cab7a65aebc347cedde2634521
qsm <348867341@qq.com>
2023-07-18 04:53:54 +0800

    init5

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,3 @@
-/*
- * @Author: qsm 348867341@qq.com
- * @Date: 2023-07-18 03:13:29
- * @LastEditors: qsm 348867341@qq.com
- * @LastEditTime: 2023-07-18 04:12:52
- * @FilePath: /eslint-react-vite-ts-template/.eslintrc.cjs
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-/* eslint-env node */
-
 module.exports = {
     root: true,
     env: { browser: true, es2021: true, node: true },
