873e188cd51693bb270177ecd795a57ec20bcbbb
qsm <348867341@qq.com>
2023-08-06 02:58:25 +0800

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,11 @@
+/*
+ * @Author: qsm 348867341@qq.com
+ * @Date: 2023-07-18 03:13:29
+ * @LastEditors: qsm 348867341@qq.com
+ * @LastEditTime: 2023-08-06 02:58:14
+ * @FilePath: /eslint-react-vite-ts-template/.eslintrc.cjs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 module.exports = {
     root: true,
     env: { browser: true, es2021: true, node: true },
@@ -12,7 +20,7 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
-        project: true,
+        project: './tsconfig.json',
         tsconfigRootDir: __dirname
     },
     plugins: ['react-refresh'],
